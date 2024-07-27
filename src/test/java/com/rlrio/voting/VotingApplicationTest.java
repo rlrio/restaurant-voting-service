ba03922c2318a22490f27ae6009fa99cae48c788
rlrio <svetlana.kras881@gmail.com>
2024-07-27 18:56:28 +0500

    init pom and configs

---
+++ src/test/java/com/rlrio/voting/VotingApplicationTest.java
@@ -0,0 +1,13 @@
+package com.rlrio.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
